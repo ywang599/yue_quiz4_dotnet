f4e5e20e0324fea0bb80ff897fcd165e49583137
Yue Wang <ywang599@my.centennialcollege.ca>
2026-04-09 10:56:15 -0400

    initial commit

---
+++ yue_quiz4_dotnet_app/yue_quiz4_dotnet_app/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using yue_quiz4_dotnet_app.Models;
+
+namespace yue_quiz4_dotnet_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ yue_quiz4_dotnet_app/yue_quiz4_dotnet_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace yue_quiz4_dotnet_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
